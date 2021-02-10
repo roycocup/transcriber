4bf68213fc685740752568e0444fd0f7ab73ad2b
Rodrigo Dias <rodrigo.dias@avantiplc.com>
2021-02-10 21:50:43 +0000

    adding file for hooks

---
+++ features/environment.py
@@ -0,0 +1,30 @@
+def before_feature(context, feature):
+    pass
+
+def after_feature(context, feature):
+    pass
+
+def before_tag(context, tag):
+    pass
+
+def before_scenario(context, scenario):
+    pass
+
+def before_step(context, step):
+    pass
+
+def after_step(context, step):
+    pass
+
+def after_scenario(context, step):
+    pass
+
+def after_tag(context, tag):
+    pass
+    
+def before_all(context):
+	pass
+    
+def after_all(context):
+	pass
+
