dbe751de5c583d0918244fead570f5accd625673
Rodrigo Dias <rodrigo.pitta@gmail.com>
2021-02-28 18:19:51 +0000

    more

--- transcriber/db.py
+++ transcriber/db.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-import transcriber
-
 engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
@@ -12,9 +10,21 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
+    # https://flask.palletsprojects.com/en/1.1.x/tutorial/database/
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    
     import transcriber.user
     Base.metadata.create_all(bind=engine)
+    db = get_db()
+
+    with transcriber.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.'
