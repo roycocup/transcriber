ef376ea5071e010b3710d447118e87e8250c784c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2021-03-01 11:34:27 +0000

    creating first template

--- web/transcriber/views.py
+++ web/transcriber/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello from view")
+    return render(request, 'transcriber/home.html')
