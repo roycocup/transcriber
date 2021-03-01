5a07b3528bf1dc03d6a2dd5a9aff25d2174c18d7
Rodrigo Dias <rodrigo.pitta@gmail.com>
2021-03-01 15:59:58 +0000

    modelling

--- web/transcriber/models.py
+++ web/transcriber/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Uploads(models.Model):
+    filename = models.CharField(max_length=254)
+    bucket = models.CharField(max_length=254)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=254)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
