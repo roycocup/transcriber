d5f1db92246f4b3e07d59b61ffdd32f187a59239
Rodrigo Dias <rodrigo.pitta@gmail.com>
2021-03-06 12:59:34 +0000

    trying to store session and ext user

--- web/transcriber/models.py
+++ web/transcriber/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import *
+from django.dispatch import receiver
 
 class Uploads(models.Model):
     filename = models.CharField(max_length=254)
@@ -8,4 +10,26 @@ class Uploads(models.Model):
     token = models.CharField(max_length=254)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+class User_Ext(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    session = models.CharField(max_length=254, null=True)
+
+
+# @receiver(pre_save, sender=User)
+# def user_save(sender, instance, **kwargs):
+#     try:
+#         user = User.objects.get(username='anonymous')
+#         ext, created = User_Ext.objects.create(user=user)
+#     except Exception as e:
+#         print(instance, kwargs, e)
+
+
+# @receiver(post_save, sender=User)
+# def user_save(sender, instance, **kwargs):
+#     try:
+#         ext, created = User_Ext.objects.get_or_create(user=sender)
+#     except Exception as e:
+#         print(instance, kwargs, e)
 
--- web/transcriber/views.py
+++ web/transcriber/views.py
@@ -1,22 +1,55 @@
 from django.shortcuts import *
 from django.http import *
+
+from hashlib import *
+import datetime as dt
+
+from logging import *
+
+from django.contrib.auth.models import *
 from .forms import UploadFileForm
+from .models import *
 
 
 def index(request):
+    if not _get_session(request):
+        _set_session(request)
+    session_value = _get_session(request)
+    
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES)
+            handle_uploaded_file(request.FILES, request)
             return HttpResponseRedirect('/')
     else:
         form = UploadFileForm()
     
-    return render(request, 'transcriber/home.html', {'form': form})
+    return render(request, 'transcriber/home.html', {'form': form, 'session':session_value })
     
 
-def handle_uploaded_file(_file):
+def handle_uploaded_file(_file, request):
     name = _file['file'].name
+        
+    user, created = User.objects.get_or_create(username='anonymous')
+    if created:
+        user.save()
+
+    ext, created = User_Ext.objects.get_or_create(user=user, session=_get_session(request))
+    if created:
+        ext.save()
+
+    Uploads(filename=name, user=user).save()
+
     with open(f'uploads/{name}', 'wb+') as destination:
         for chunk in _file['file'].chunks():
             destination.write(chunk)
+
+def _set_session(request):
+    now = dt.datetime.now()
+    now = bytes(str(now), encoding='utf-8')
+    # if its not alredy set
+    if not _get_session(request):
+        request.session['user']  = md5(now).hexdigest()
+
+def _get_session(request):
+    return request.session.get('user', None)
